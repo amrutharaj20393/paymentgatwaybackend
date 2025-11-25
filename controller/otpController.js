8b063f340960fe5c943f9ff3d8727ba5c212b12f
amrutharaj20393 <amrutharaj20393@gmail.com>
2025-11-25 22:01:21 +0530

    added

--- controller/otpController.js
+++ controller/otpController.js
@@ -5,51 +5,36 @@ const dotenv = require('dotenv')
 dotenv.config()
 const { Resend } = require("resend");
 exports.otpamazonController = async (req, res) => {
-  //console.log("hau")
-  //  const email = req.query.email
-
-  // const { email } = req.body;
-  // const existinguser = await amazonusers.findOne({ email })
-  // console.log(existinguser)
-  // if (existinguser) {
-  //   const otp = Math.floor(100000 + Math.random() * 900000);
-
-  //   const transporter = nodemailer.createTransport({
-  //     service: "gmail",
-  //     auth: {
-  //       user: process.env.MAIL_USER,
-  //       pass: process.env.MAIL_PASS,
-  //     },
-  //   });
-
-  //   await transporter.sendMail({
-  //     from: "amrutharaj20393@gmail.com",
-  //     to: email,
-  //     subject: "Your OTP Code",
-  //     text: `Your OTP is: ${otp}`,
-  //   });
-
-  //   res.status(200).json({ success: true, otp });
-  // }
-
-  // else {
-  //   res.status(404).json("incorrect email or password")
-  // }
+  console.log("hau")
+   //const email = req.query.email
 
   const { email } = req.body;
-  const otp = Math.floor(100000 + Math.random() * 900000);
+  const existinguser = await amazonusers.findOne({ email })
+  console.log(existinguser)
+  if (existinguser) {
+    const otp = Math.floor(100000 + Math.random() * 900000);
 
-  try {
-    await resend.emails.send({
-      from: "Your App <onboarding@resend.dev>",
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: "amrutharaj20393@gmail.com",
       to: email,
-      subject: "Your OTP",
+      subject: "Your OTP Code",
       text: `Your OTP is: ${otp}`,
     });
 
-    res.status(200).json({ success: true, message: "OTP sent", otp });
-  } catch (error) {
-    res.status(500).json(error);
+    res.status(200).json({ success: true, otp });
   }
 
+  else {
+    res.status(404).json("incorrect email or password")
+  }
+
+ 
 }
